614ab68c1612314bd947fee55531a5c84af50415
Rahadian Dewandono <gusdewa@gmail.com>
2017-01-15 00:13:14 +0800

    Update firebase

--- tools/config.js
+++ tools/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  title: 'React Static Boilerplate',        // Your website title
-  url: 'https://rsb.kriasoft.com',          // Your website URL
-  project: 'react-static-boilerplate',      // Firebase project. See README.md -> How to Deploy
+  title: 'Multistores',        // Your website title
+  url: 'https://multistores-35b17.firebaseapp.com/',          // Your website URL
+  project: 'multistores-35b17',      // Firebase project. See README.md -> How to Deploy
   trackingID: 'UA-XXXXX-Y',                 // Google Analytics Site's ID
 };
